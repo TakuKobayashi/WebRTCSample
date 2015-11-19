dc05db3e0c739fbb489e4211aebacc60e344878a
Taku Kobayashi <taku.kobayashi@gamepot.co.jp>
2015-11-19 19:11:40 +0900

    first commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+//サーバーの立ち上げ
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3001;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+//指定したポートにきたリクエストを受け取れるようにする
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
